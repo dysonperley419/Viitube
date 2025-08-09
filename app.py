579669115d80847ba2e1a4ebda0e341d1db6b108
dysonperley419 <perleydyson903@gmail.com>
2025-08-09 00:27:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4361,4 +4361,5 @@ def recently_featured(region):
 
 # === Run ===
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+
+    app.run(debug=True, host='0.0.0.0', port=443)
